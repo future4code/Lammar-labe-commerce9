d58364eeb084db9fbb3b6810ffa9c362400b3360
gans92 <gabri.alisson.23@gmail.com>
2022-07-15 17:00:28 -0500

    começo do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Home from './components/Home/Home';
+import Filtro from './components/Filtro/Filtro';
+import Carrinho from './components/Carrinho/Carrinho';
+import styled from 'styled-components';
 
-function App() {
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`
+
+const App = () => {
+
+  const [valorMinimo, setValorMinimo] = useState(0);
+  const [valorMaximo, setValorMaximo] = useState(100);
+  const [buscar, setBuscar] = useState("");
+
   return (
-    <div>
-    
-    </div>
+    <>
+      <AppContainer>
+        <Filtro
+        valorMaximo={valorMaximo}
+        valorMinimo={valorMinimo}
+        OnChangeValorMinimo={(e) => setValorMinimo(e.target.value)}
+        OnChangeValorMaximo={(e) => setValorMaximo(e.target.value)}
+        OnChangeBuscar={(e) => setBuscar(e.target.value)}/>
+        <Home
+        valorMaximo={valorMaximo}
+        valorMinimo={valorMinimo}
+        buscar={buscar}/>
+        <Carrinho/>
+      </AppContainer>
+    </>
   );
 }
 
---
+++ src/components/MockDeDados.js
@@ -0,0 +1,55 @@
+const produtos = [
+    {
+      id: 1,
+      imagem:
+        "",
+      titulo: "",
+      preco: 1,
+      quantidade: 1,
+    },
+
+    {
+        id: 1,
+        imagem:
+          "",
+        titulo: "",
+        preco: 1,
+        quantidade: 1,
+      },
+
+      {
+        id: 1,
+        imagem:
+          "",
+        titulo: "",
+        preco: 1,
+        quantidade: 1,
+      },
+
+      {
+        id: 1,
+        imagem:
+          "",
+        titulo: "",
+        preco: 1,
+        quantidade: 1,
+      },
+
+      {
+        id: 1,
+        imagem:
+          "",
+        titulo: "",
+        preco: 1,
+        quantidade: 1,
+      },
+
+      {
+        id: 1,
+        imagem:
+          "",
+        titulo: "",
+        preco: 1,
+        quantidade: 1,
+      },
+  ];
