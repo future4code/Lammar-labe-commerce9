d871752f06b47e9173e9c75e97c0b770c6bd523d
jarbasmarinho33 <jarbasmarinho33@gmail.com>
2022-07-16 15:24:47 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div>
-    
-    </div>
+<>
+<Home/>
+</>
   );
 }
-
 export default App;
